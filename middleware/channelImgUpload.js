cc94cb002dd63bd2c50178ccf0927698b8503389
jochongs <joch2712@naver.com>
2023-01-14 21:40:40 +0900

    update s3 module

--- middleware/channelImgUpload.js
+++ middleware/channelImgUpload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3-transform');
 const channelDataValidCheck = require('../module/channelDataValidCheck');
-const s3 = require('../module/s3');
+const s3 = require('s3');
 const AWS = require('aws-sdk');
 const awsConfig = require('../config/awsConfig');
 
