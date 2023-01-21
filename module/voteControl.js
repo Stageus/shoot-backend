8d5a06852ac26f5a8d08d75feb10a64d6db44818
jochongs <=>
2023-01-21 16:16:58 +0900

    ADD DELETE /vote/{vote-idx} API

--- module/voteControl.js
+++ module/voteControl.js
@@ -57,37 +57,42 @@ const deleteVote = (voteIdx, loginUserEmail) => {
         try{
             await pgClient.connect();
 
-            //BEGIN
-            await pgClient.query('BEGIN');
+            //SELECT
+            const selectVoteSql = 'SELECT * FROM shoot.post_contents_vote WHERE vote_idx = $1';
+            const selectVoteResult = await pgClient.query(selectVoteSql, [voteIdx]);
 
-            //INSERT vote_channel
-            const insertVoteChannelSql = 'INSERT INTO shoot.vote_channel (vote_idx, email) VALUES ($1, $2)';
-            await pgClient.query(insertVoteChannelSql, [voteIdx, loginUserEmail]);
-
-            //UPDATE post_contents_vote
-            const updateVoteSql = 'UPDATE shoot.post_contents_vote SET vote_count = vote_count + 1 WHERE vote_idx = $1';
-            await pgClient.query(updateVoteSql, [voteIdx]);
-
-            //COMMIT
-            await pgClient.query('COMMIT');
-
-            resolve(1);
-        }catch(err){
-            await pgClient.query('ROLLBACK');
-
-            if(err.code == 23505){  //unique error
-                reject({
-                    statusCode : 403,
-                    message : 'already vote'
-                });
-                return;
-            }else if(err.code == 23503){ //no vote-idx error
+            if(selectVoteResult.rows.length === 0){
                 reject({
                     statusCode : 404,
                     message : 'cannot find vote'
                 });
                 return;
             }
+
+            //BEGIN
+            await pgClient.query('BEGIN');
+
+            //DELETE
+            const deleteVoteChannelSql = 'DELETE FROM shoot.vote_channel WHERE vote_idx = $1 AND email = $2';
+            const deleteVoteChannelResult = await pgClient.query(deleteVoteChannelSql, [voteIdx, loginUserEmail]);
+
+            if(deleteVoteChannelResult.rowCount !== 0){
+                //UPDATE post_contents_vote
+                const updateVoteSql = 'UPDATE shoot.post_contents_vote SET vote_count = vote_count - 1 WHERE vote_idx = $1';
+                await pgClient.query(updateVoteSql, [voteIdx]);
+
+                //COMMIT
+                await pgClient.query('COMMIT');
+
+                resolve(1);
+            }else{
+                reject({
+                    statusCode : 403,
+                    message : 'do not have vote channel record'
+                });
+            }
+        }catch(err){
+            await pgClient.query('ROLLBACK');
 
             reject({
                 statusCode : 409,
--- routes/vote.js
+++ routes/vote.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const loginAuth = require('../middleware/loginAuth');
-const { addVote } = require('../module/voteControl');
+const { addVote, deleteVote } = require('../module/voteControl');
 
 router.post('/:voteIdx', loginAuth, async (req, res) => {
     //from FE
@@ -14,6 +14,29 @@ router.post('/:voteIdx', loginAuth, async (req, res) => {
     //main
     try{
         await addVote(voteIdx, loginUserEmail);
+    }catch(err){
+        err.err ? console.log(err.err) : null;
+
+        result.message = err.message;
+        statusCode = err.statusCode;
+    }
+
+    //send result
+    res.status(statusCode).send(result);
+});
+
+router.delete('/:voteIdx', loginAuth, async (req, res) => {
+    //from FE
+    const voteIdx = req.params.voteIdx;
+    const loginUserEmail = req.email;
+
+    //to FE
+    const result = {};
+    let statusCode = 200;
+
+    //main
+    try{
+        await deleteVote(voteIdx, loginUserEmail);
     }catch(err){
         err.err ? console.log(err.err) : null;
 
